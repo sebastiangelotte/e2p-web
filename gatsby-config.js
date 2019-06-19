5c8b9d30ae823f8cf0db596ae12102fee00c3990
Sebastian Gelotte <sebastian.gelotte@gmail.com>
2019-06-19 23:57:03 +0200

    Testing netlify identity

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,12 @@ module.exports = {
         spaceId: process.env.CONTENTFUL_SPACE_ID,
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
       }
+    },
+    {
+      resolve: `gatsby-plugin-netlify-identity`,
+      options: {
+        url: `https://pedantic-morse-58901e.netlify.com/` // required!
+      }
     }
   ]
 }
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,70 +1,20 @@
 import React from 'react'
 import Navigation from './navigation/navigation'
 import Footer from './footer/footer'
+import IdentityModal, { useIdentityContext } from 'react-netlify-identity-widget'
+import 'react-netlify-identity-widget/styles.css'
 
 import '../styles/main.scss'
 
-const firebase = require("firebase/app")
-require("firebase/auth")
-
-if (typeof window !== 'undefined') {
-  
-  const firebaseConfig = {
-    apiKey: 'AIzaSyAb0XyD6AgOfybvYqAE-pkS4sPyrMdqh38',
-    authDomain: 'e2p-web.firebaseapp.com',
-    databaseURL: 'https://e2p-web.firebaseio.com',
-    projectId: 'e2p-web',
-    storageBucket: 'e2p-web.appspot.com',
-    messagingSenderId: '226326778642',
-  }
-  
-  firebase.initializeApp(firebaseConfig)
-
-  var provider = new firebase.auth.GoogleAuthProvider()
-
-}
-
-var token
-var user = 'poo'
-
-const signIn = () => {
-  console.log(user)
-  firebase.auth().signInWithPopup(provider).then(result => {
-    token = result.credential.accessToken
-    user = result.user
-  }).catch(error => {
-    console.error(error.code, error.message)
-  })
-  console.log(user)
-}
-
-const signOut = () => {
-  firebase.auth().signOut().then(() => {
-    console.log('Loggedout!')
-  }).catch(error => {
-    console.error(error)
-  })
-}
-
-const Layout = props => {
+const Layout = ({ children }) => {
   return (
     <div id="outer-container">
-    <Navigation />
-    <main id="page-wrap">
-      {
-        user
-          ? <p>Hello, {user.displayName}</p>
-          : <p>Please sign in.</p>
-      }
-      {
-        user
-          ? <button onClick={signOut}>Sign out</button>
-          : <button onClick={signIn}>Sign in with Google</button>
-      }
-      {props.children}
-      <Footer />
-    </main>
-  </div>
+      <Navigation />
+      <main id="page-wrap">
+        {children}
+        <Footer />
+      </main>
+    </div>
   )
 }
 
