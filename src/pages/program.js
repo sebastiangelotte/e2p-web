92027c5c94c930a2361587165e84c133836b1016
Sebastian Gelotte <sebastian.gelotte@gmail.com>
2020-01-12 22:40:38 +0100

    Add content and images for program page

--- src/pages/program.js
+++ src/pages/program.js
@@ -1,38 +1,19 @@
-import React, { useState } from "react"
-import { graphql, useStaticQuery, Link } from "gatsby"
+import React from "react"
+import styled from "styled-components"
+import { graphql, useStaticQuery } from "gatsby"
 
-import {
-  Icon,
-  Segment,
-  Container,
-  Header,
-  Label,
-  Button,
-} from "semantic-ui-react"
+import { Segment, Header } from "semantic-ui-react"
 
 import Head from "../components/head"
 import Layout from "../components/layout"
-import Filter from "../components/filter"
 import Hero from "../components/hero"
+import scienceSVG from "../images/undraw-science.svg"
+import communitySVG from "../images/undraw-community.svg"
+import financeSVG from "../images/undraw-finance.svg"
 
-const Services = () => {
+const Program = () => {
   const data = useStaticQuery(graphql`
     query {
-      allContentfulService {
-        edges {
-          node {
-            slug
-            title
-            tags
-            shortDescription {
-              shortDescription
-            }
-            description {
-              json
-            }
-          }
-        }
-      }
       file(relativePath: { eq: "services.jpg" }) {
         childImageSharp {
           # Specify the image processing specifications right in the query.
@@ -44,16 +25,6 @@ const Services = () => {
       }
     }
   `)
-
-  const [services, setServices] = useState(data.allContentfulService.edges)
-
-  const updateServices = services => {
-    if (services.length > 0) {
-      setServices(services)
-    } else {
-      setServices(data.allContentfulService.edges) // reset state
-    }
-  }
 
   return (
     <Layout transparentNavigation>
@@ -78,44 +49,127 @@ const Services = () => {
           </p>
         </div>
       </Hero>
-      <Segment vertical>
-        {/* {services.map((service, index) => {
-          return (
-            <Segment
-              textAlign="center"
-              vertical
-              style={{ marginBottom: "40px", paddingBottom: "40px" }}
-            >
-              <Container text>
-                {service.node.tags &&
-                  service.node.tags.map(tag => {
-                    return (
-                      <Label key={tag} size="large">
-                        <Icon name="tag" /> {tag}
-                      </Label>
-                    )
-                  })}
-                <Header as="h2" style={{ marginTop: "15px" }}>
-                  {service.node.title}
-                </Header>
-                <div style={{ marginBottom: "20px" }}>
-                  {service.node.shortDescription.shortDescription}
-                </div>
-                <Link to={`/services/${service.node.slug}`}>
-                  <Button
-                    positive
-                    content="Läs mer"
-                    icon="arrow right"
-                    labelPosition="left"
-                  />
-                </Link>
-              </Container>
-            </Segment>
-          )
-        })} */}
+      <Segment vertical style={{ padding: "0 20px" }}>
+        <TopWrapper>
+          <h2>5 stegs projektmodell för utveckling</h2>
+          <p>
+            Vi har en 5 stegs projektmodell som möjliggör kostnadseffektiv
+            utveckling av merkomplexa utbildningsprogram, från idé till färdig
+            lösning.
+          </p>
+          <p>
+            Vi börjar med en förstudie <b>(1)</b> innehållande en GAP-analys{" "}
+            <b>(1)</b> för att fastställa det potentiella kunskapslyftet hos den
+            grupp av individer som man önskar utveckla. Utvecklas en
+            kravspecifikation <b>(2)</b>, med tydliga effektmål och
+            fokusområden, allt utifrån kundens förutsättningar och budget.
+            Aktuella utvecklingskonsulter kontaktas och kvalitetssäkras{" "}
+            <b>(3)</b> utifrån önskat behov av kompetens- & erfarenhetsprofil.
+            En tid- & projektplan <b>(4)</b> fastställs och struktur och
+            innehållsblock i det planerade utbildningsprogrammet utformas{" "}
+            <b>(5)</b>.
+          </p>
+        </TopWrapper>
+        <div style={{ backgroundColor: "#f7f7f7" }}>
+          <ExampleWrapper>
+            <img src={scienceSVG} alt="Utbildningsprogram" />
+            <div>
+              <h3>Utbildningsprogram för 1:a och 2:a linjens chefer</h3>
+              <p>
+                Utveckling av ledarutbildning för chefer och ledare verksamma
+                inom kemiindustrin. Målsättningen var att stärka kompetensen
+                inom tre huvudsakliga områden,
+              </p>
+              <ul>
+                <li>Ledarskap</li>
+                <li>Målstyrning</li>
+                <li>Coaching</li>
+              </ul>
+              <p>
+                Utbildningen genomfördes som i tre etapper á 2-3 dagar under en
+                6 månaders period med diskussioner och uppgifter emellan.
+              </p>
+            </div>
+          </ExampleWrapper>
+          <ExampleWrapper>
+            <div>
+              <h3>Ledarutveckling och individuell coaching för enhetschefer</h3>
+              <p>
+                Utveckling av upplägg för coaching och stöttning av enhetschefer
+                inom Transportindustrin. Målsättningen var att,
+              </p>
+              <ul>
+                <li>
+                  Utveckla och stärka chefer och ledare inom organisationen
+                </li>
+                <li>
+                  Möjliggöra erfarenhetsutbyte och kunskapsspridning mellan
+                  enheterna
+                </li>
+              </ul>
+              <p>
+                Utbildningen genomfördes som grupputveckling och individuell
+                coaching under en 8 månaders period.
+              </p>
+            </div>
+            <img src={communitySVG} alt="Ledarutveckling" />
+          </ExampleWrapper>
+          <ExampleWrapper>
+            <img src={financeSVG} alt="Utveckling av kursupplägg" />
+            <div>
+              <h3>Utbildning inom entreprenadjuridik för jurister</h3>
+              <p>
+                Utveckling av kursupplägg för företagsekonomer och jurister
+                verksamma inom petrokemindustrin. Målsättningen var att skapa en
+                nischad utbildning med fokus på spelregler och tolkning av
+                avtal.
+              </p>
+              <p>
+                Utbildningen genomfördes som grupputveckling applicerat på ett
+                antal praktiska case där möjlighet till juridiskt stöd och
+                konsultation efteråt fanns att tillgå.
+              </p>
+            </div>
+          </ExampleWrapper>
+        </div>
       </Segment>
     </Layout>
   )
 }
 
-export default Services
+export default Program
+
+const TopWrapper = styled.div`
+  text-align: center;
+  max-width: 1000px;
+  margin: 0 auto;
+  padding: 50px 0;
+
+  p {
+    font-size: 120%;
+    line-height: 1.7em;
+  }
+`
+
+const ExampleWrapper = styled.div`
+  max-width: 1600px;
+  margin: 0 auto;
+  padding: 50px 0;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  font-size: 120%;
+
+  > * {
+    padding-right: 80px;
+  }
+
+  img {
+    width: 50%;
+    max-height: 500px;
+  }
+
+  @media only screen and (max-width: 800px) {
+    flex-direction: column;
+  }
+`
