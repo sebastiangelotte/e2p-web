0788d7ecb0b9a9622902df010a2243bd9cc04bba
Sebastian Gelotte <sebastian.gelotte@gmail.com>
2020-09-05 20:58:03 +0200

    Remove message (debug)

--- src/components/courseSignup.js
+++ src/components/courseSignup.js
@@ -47,165 +47,159 @@ const CourseSignup = ({ courseName, courseDates }) => {
 
   return (
     <>
-      {message.isVisible ? (
-        <Message
-          negative={message.negative}
-          positive={message.positive}
-          header={message.header}
-          content={message.content}
+      {/* {message.isVisible ? ( */}
+      {/*   <Message */}
+      {/*     negative={message.negative} */}
+      {/*     positive={message.positive} */}
+      {/*     header={message.header} */}
+      {/*     content={message.content} */}
+      {/*   /> */}
+      {/* ) : ( */}
+      <Form
+        id="form-signup"
+        name="courseSignup"
+        onSubmit={event => handleSubmit(event)}
+        data-netlify="true"
+      >
+        {/* needed for netlify */}
+        <input type="hidden" name="form-name" value="courseSignup" />
+        <input type="hidden" name="Kurs" value={courseName} />
+        <Form.Group widths="equal">
+          <select
+            id="date"
+            label="Datum"
+            name="Datum"
+            value={date}
+            onBlur={e => setDate(e.target.value)}
+          >
+            {courseDates.map(tillfalle => (
+              <option
+                value={tillfalle.date}
+              >{`${tillfalle.city}: ${tillfalle.date}`}</option>
+            ))}
+          </select>
+        </Form.Group>
+        <h3>Deltagare</h3>
+        <Form.Group widths="equal">
+          <Form.Input
+            fluid
+            label="Namn"
+            placeholder="Namn"
+            required
+            name="namn"
+          />
+          <Form.Input
+            type="email"
+            fluid
+            label="E-post"
+            placeholder="E-post"
+            required
+            name="email"
+            value={email}
+            onInput={e => setEmail(e.target.value)}
+          />
+        </Form.Group>
+        {Array.apply(null, { length: extraParticipants - 1 }).map(
+          (_, index) => (
+            <Form.Group widths="equal" key={index}>
+              <Form.Input
+                fluid
+                label="Namn"
+                placeholder="Namn"
+                required
+                name={`namn${index}`}
+              />
+              <Form.Input
+                type="email"
+                fluid
+                label="E-post"
+                placeholder="E-post"
+                required
+                name={`email${index}`}
+              />
+            </Form.Group>
+          )
+        )}
+        <Button
+          content="Anmäl fler deltagare"
+          icon="plus"
+          labelPosition="left"
+          onClick={() => {
+            setExtraParticipants(extraParticipants + 1)
+          }}
         />
-      ) : (
-        <Form
-          id="form-signup"
-          name="courseSignup"
-          onSubmit={event => handleSubmit(event)}
-          data-netlify="true"
-        >
-          {/* needed for netlify */}
-          <input type="hidden" name="form-name" value="courseSignup" />
-          <input type="hidden" name="Kurs" value={courseName} />
-          <Form.Group widths="equal">
-            <select
-              id="date"
-              label="Datum"
-              name="Datum"
-              value={date}
-              onBlur={e => setDate(e.target.value)}
-            >
-              {courseDates.map(tillfalle => (
-                <option
-                  value={tillfalle.date}
-                >{`${tillfalle.city}: ${tillfalle.date}`}</option>
-              ))}
-            </select>
-          </Form.Group>
-          <h3>Deltagare</h3>
-          <Form.Group widths="equal">
-            <Form.Input
-              fluid
-              label="Namn"
-              placeholder="Namn"
-              required
-              name="namn"
-            />
-            <Form.Input
-              type="email"
-              fluid
-              label="E-post"
-              placeholder="E-post"
-              required
-              name="email"
-              value={email}
-              onInput={e => setEmail(e.target.value)}
-            />
-          </Form.Group>
-          {Array.apply(null, { length: extraParticipants - 1 }).map(
-            (_, index) => (
-              <Form.Group widths="equal" key={index}>
-                <Form.Input
-                  fluid
-                  label="Namn"
-                  placeholder="Namn"
-                  required
-                  name={`namn${index}`}
-                />
-                <Form.Input
-                  type="email"
-                  fluid
-                  label="E-post"
-                  placeholder="E-post"
-                  required
-                  name={`email${index}`}
-                />
-              </Form.Group>
-            )
-          )}
+        <h3>Faktureringsinformation</h3>
+        <Form.Group widths="equal">
+          <Form.Input fluid label="Företag" placeholder="Företag" required />
+          <Form.Input
+            fluid
+            label="Fakturaadress"
+            placeholder="Fakturaadress"
+            required
+            name="fakturaadress"
+          />
+          <Form.Input
+            fluid
+            label="Postnummer"
+            placeholder="Postnummer"
+            required
+            name="postnummer"
+          />
+          <Form.Input fluid label="Ort" placeholder="Ort" required name="ort" />
+        </Form.Group>
+        {!showContactPerson ? (
           <Button
-            content="Anmäl fler deltagare"
-            icon="plus"
-            labelPosition="left"
-            onClick={() => {
-              setExtraParticipants(extraParticipants + 1)
+            size="small"
+            onClick={e => {
+              e.preventDefault()
+              setShowContactPerson(true)
             }}
-          />
-          <h3>Faktureringsinformation</h3>
-          <Form.Group widths="equal">
-            <Form.Input fluid label="Företag" placeholder="Företag" required />
-            <Form.Input
-              fluid
-              label="Fakturaadress"
-              placeholder="Fakturaadress"
-              required
-              name="fakturaadress"
-            />
-            <Form.Input
-              fluid
-              label="Postnummer"
-              placeholder="Postnummer"
-              required
-              name="postnummer"
-            />
-            <Form.Input
-              fluid
-              label="Ort"
-              placeholder="Ort"
-              required
-              name="ort"
-            />
-          </Form.Group>
-          {!showContactPerson ? (
-            <Button
-              size="small"
-              onClick={e => {
-                e.preventDefault()
-                setShowContactPerson(true)
-              }}
-            >
-              Annan kontaktperson?
-            </Button>
-          ) : (
-            <>
-              <h3>Kontaktperson</h3>
-              <Form.Group widths="equal">
-                <Form.Input
-                  fluid
-                  label="Namn"
-                  placeholder="Namn"
-                  name="kontaktperson-namn"
-                />
-                <Form.Input
-                  fluid
-                  label="E-post"
-                  placeholder="E-post"
-                  name="kontaktperson-epost"
-                />
-                <Form.Input fluid label="Postnummer" placeholder="Postnummer" />
-                <Form.Input
-                  fluid
-                  label="Ort"
-                  placeholder="Ort"
-                  name="kontaktperson-ort"
-                />
-              </Form.Group>
-            </>
-          )}
+          >
+            Annan kontaktperson?
+          </Button>
+        ) : (
+          <>
+            <h3>Kontaktperson</h3>
+            <Form.Group widths="equal">
+              <Form.Input
+                fluid
+                label="Namn"
+                placeholder="Namn"
+                name="kontaktperson-namn"
+              />
+              <Form.Input
+                fluid
+                label="E-post"
+                placeholder="E-post"
+                name="kontaktperson-epost"
+              />
+              <Form.Input fluid label="Postnummer" placeholder="Postnummer" />
+              <Form.Input
+                fluid
+                label="Ort"
+                placeholder="Ort"
+                name="kontaktperson-ort"
+              />
+            </Form.Group>
+          </>
+        )}
 
-          <h3>Övrigt</h3>
-          <Form.TextArea
-            placeholder="Allergier, speciella önskemål m.m."
-            name="ovrigt"
-          />
-          <Button
-            loading={isLoading}
-            type="submit"
-            value="Send"
-            content="Skicka anmälan"
-            icon="send"
-            labelPosition="left"
-            positive
-          />
-        </Form>
-      )}
+        <h3>Övrigt</h3>
+        <Form.TextArea
+          placeholder="Allergier, speciella önskemål m.m."
+          name="ovrigt"
+        />
+        <Button
+          loading={isLoading}
+          type="submit"
+          value="Send"
+          content="Skicka anmälan"
+          icon="send"
+          labelPosition="left"
+          positive
+        />
+      </Form>
+      {/* )} */}
     </>
   )
 }
