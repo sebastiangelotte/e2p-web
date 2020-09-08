0807414df8ffc3d68e7d97a7ddd46c28643d0122
Sebastian Gelotte <sebastian.gelotte@gmail.com>
2020-09-08 19:46:41 +0200

    framer-motion components

---
+++ src/components/scaleBox.js
@@ -0,0 +1,45 @@
+import React, { useMemo, useContext } from "react"
+
+import { IntersectionContext } from "./intersectionObserver"
+import { motion } from "framer-motion"
+
+export const ScaleBox = ({
+  children,
+  delayOrder, // order of appearance
+  duration = 0.4,
+  easing = [0.42, 0, 0.58, 1],
+}) => {
+  const { inView } = useContext(IntersectionContext)
+  const transition = useMemo(
+    () => ({
+      duration,
+      delay: delayOrder / 5,
+      ease: easing,
+    }),
+    [duration, delayOrder, easing]
+  )
+
+  const variants = {
+    hidden: {
+      y: "10px",
+      opacity: 0,
+      transition,
+    },
+    show: {
+      y: "0px",
+      opacity: 1,
+      transition,
+    },
+  }
+
+  return (
+    <motion.div
+      initial="hidden"
+      animate={inView ? "show" : "hidden"}
+      exit="hidden"
+      variants={variants}
+    >
+      {children}
+    </motion.div>
+  )
+}
