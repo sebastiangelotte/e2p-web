d297e8b76b8ecb4efed815a9640410da9db2593d
Sebastian Gelotte <sebastian.gelotte@gmail.com>
2020-10-11 01:24:53 +0200

    Coaching

---
+++ src/components/coachingForm.js
@@ -0,0 +1,109 @@
+import React, { useState } from "react"
+import styled from "styled-components"
+import { Button } from "../components/new/styledComponents"
+
+const CoachingForm = ({ source }) => {
+  const [isLoading, setIsLoading] = useState(false)
+  const [message, setMessage] = useState({
+    isVisible: false,
+    header: "Ett meddelande",
+    content: "",
+    negative: false,
+    positive: false,
+  })
+
+  const handleSubmit = event => {
+    setIsLoading(true)
+    event.preventDefault()
+    const formData = new FormData(event.target)
+    const searchParams = new URLSearchParams(formData).toString() // create URL params
+
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: searchParams,
+      dataType: "json",
+      mode: "no-cors",
+    })
+      .then(() => {
+        setIsLoading(false)
+        setMessage({
+          isVisible: true,
+          header: "Tack för ditt intresse!",
+          content: "Vi återkommer så snart vi kan.",
+          positive: true,
+        })
+        document.getElementById("form").reset() // reset form after submit
+      })
+      .catch(err => {
+        console.log(err)
+        setIsLoading(false)
+      })
+  }
+
+  return (
+    <>
+      {message.isVisible ? (
+        <Message>
+          <h3>{message.header}</h3>
+          <p>{message.content}</p>
+        </Message>
+      ) : (
+        <Form
+          id="form"
+          name="coaching"
+          onSubmit={event => handleSubmit(event)}
+          data-netlify="true"
+        >
+          {/* needed for netlify */}
+          <input type="hidden" name="form-name" value="contact" />
+          <input type="hidden" name="Skickat från" value={source} />
+          <Section widths="equal">
+            <input type="text" label="Namn" placeholder="Namn" name="namn" />
+            <input type="email" placeholder="E-post *" required name="email" />
+          </Section>
+          <textarea
+            placeholder="Dina mål, utmaningar, önskemål"
+            name="Meddelande"
+          />
+          <SubmitButton type="submit" value="Send">
+            Anmäl intresse
+          </SubmitButton>
+        </Form>
+      )}
+    </>
+  )
+}
+
+export default CoachingForm
+
+const Form = styled.form`
+  max-width: 680px;
+  padding-top: 20px;
+`
+
+const Section = styled.div`
+  margin-bottom: 20px;
+
+  h3 {
+    font-size: 20px;
+  }
+`
+
+const SubmitButton = styled(Button)`
+  background: linear-gradient(180deg, #fbc917 0%, #ff8364 100%);
+  border: none;
+  font-size: 18px;
+  font-weight: bold;
+  padding: 18px 45px 16px 45px;
+  width: 100%;
+`
+
+const Message = styled.div`
+  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  height: 100%;
+`
---
+++ src/pages/coaching.js
@@ -0,0 +1,124 @@
+import React from "react"
+import Head from "../components/head"
+import Layout from "../components/layout"
+import styled from "styled-components"
+import bg from "../images/hero-bg.svg"
+import {
+  Section,
+  Heading,
+  SectionWithBackgroundImage,
+  Inner,
+} from "../components/new/styledComponents"
+import { BsPersonBoundingBox } from "react-icons/bs"
+import { FaHandHoldingHeart, FaMagic } from "react-icons/fa"
+import Card from "../components/new/card"
+import CoachingForm from "../components/coachingForm"
+
+const Coaching = () => {
+  return (
+    <Layout transparentNavigation>
+      <Head title="Coaching" />
+
+      <SectionWithBackgroundImage background backgroundImage={bg} firstSection>
+        <StyledInner>
+          <div>
+            <Heading as="h1" inverted italic>
+              Individuell coaching
+            </Heading>
+            <p>
+              Individuell coaching innebär att du är i fokus. Målet med
+              coachingen är att ge dig stöd och utveckling i din yrkesroll. Vi
+              matchar ditt behov med våra konsulters erfarenhet av liknande
+              uppdrag.
+            </p>
+            <p>
+              Du väljer själv i vilken omfattning du önskar coaching, från något
+              enstaka tillfälle till ett antal möten under en 6 till
+              12månadersperiod.
+            </p>
+          </div>
+          <CoachingForm source="Coaching" />
+        </StyledInner>
+        {/* <StyledSection background> */}
+        <Grid>
+          <CardWrapper withBackground>
+            <FaMagic />
+            <h3>Få ut ännu mer</h3>
+            <p>
+              Få individuell coaching som anpassas efter dina behov, önskemål
+              och uppsatta mål
+            </p>
+          </CardWrapper>
+          <CardWrapper withBackground>
+            <BsPersonBoundingBox />
+            <h3>All fokus på dig</h3>
+            <p>
+              All fokus på dig och gott om tid för dina frågor och utmaningar
+            </p>
+          </CardWrapper>
+          <CardWrapper withBackground>
+            <FaHandHoldingHeart />
+            <h3>Handplockad kompetens</h3>
+            <p>Vi handplockar en konsult med lång erfarenhet inom området</p>
+          </CardWrapper>
+        </Grid>
+      </SectionWithBackgroundImage>
+      {/* </StyledSection> */}
+    </Layout>
+  )
+}
+
+export default Coaching
+
+const StyledSection = styled(Section)`
+  p {
+    font-size: 20px;
+  }
+
+  h2,
+  h3,
+  h4,
+  h5 {
+    margin-top: 40px;
+  }
+`
+
+const StyledInner = styled(Inner)`
+  display: grid;
+  grid-gap: 50px;
+  grid-template-columns: 2fr 1fr;
+  padding-bottom: 100px;
+
+  p {
+    font-size: 20px;
+  }
+
+  @media screen and (max-width: 900px) {
+    grid-template-columns: 1fr;
+  }
+`
+
+const Grid = styled(Inner)`
+  padding-bottom: 100px;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-gap: 40px;
+
+  @media screen and (max-width: 900px) {
+    grid-template-columns: 1fr;
+  }
+`
+
+const CardWrapper = styled(Card)`
+  text-align: center;
+  padding: 65px 50px 40px 50px;
+
+  svg {
+    font-size: 60px;
+    margin-bottom: 25px;
+  }
+
+  h3 {
+    font-size: 25px;
+  }
+`
