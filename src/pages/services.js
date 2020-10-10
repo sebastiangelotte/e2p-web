aa6d7a797e5b73a5ab4d9e36b97df7b3732639af
Sebastian Gelotte <sebastian.gelotte@gmail.com>
2020-10-10 20:42:24 +0200

    services page

--- src/pages/services.js
+++ src/pages/services.js
@@ -1,19 +1,18 @@
 import React, { useState } from "react"
 import { graphql, useStaticQuery, Link } from "gatsby"
-
-import {
-  Icon,
-  Segment,
-  Container,
-  Header,
-  Label,
-  Button,
-} from "semantic-ui-react"
-
+import styled from "styled-components"
 import Head from "../components/head"
 import Layout from "../components/layout"
-import Filter from "../components/filter"
-import Hero from "../components/hero"
+import {
+  Heading,
+  SectionWithBackgroundImage,
+  Section,
+  Inner,
+  Button,
+  Tag,
+} from "../components/new/styledComponents"
+import bg from "../images/hero-bg.svg"
+import { BsFillTagFill } from "react-icons/bs"
 
 const Services = () => {
   const data = useStaticQuery(graphql`
@@ -33,15 +32,6 @@ const Services = () => {
           }
         }
       }
-      file(relativePath: { eq: "services.jpg" }) {
-        childImageSharp {
-          # Specify the image processing specifications right in the query.
-          # Makes it trivial to update as your page's design changes.
-          fluid(maxHeight: 1000) {
-            ...GatsbyImageSharpFluid
-          }
-        }
-      }
     }
   `)
 
@@ -50,55 +40,70 @@ const Services = () => {
   return (
     <Layout transparentNavigation>
       <Head title="Verktyg" />
-      <Hero backgroundImage={data.file.childImageSharp.fluid}>
-        <Header as="h1" inverted>
-          Organisation & ledarutveckling
-        </Header>
-        <div>
+      <SectionWithBackgroundImage backgroundImage={bg} inverted firstSection>
+        <Inner>
+          <Heading as="h1" inverted>
+            Organisation & ledarutveckling
+          </Heading>
           <p>
             Genom vårt omfattande konsultnätverk kan vi erbjuda konsulttjänster
             för utveckling av organisation och ledare.
           </p>
-        </div>
-      </Hero>
-      <Segment vertical>
-        {services.map((service, index) => {
-          return (
-            <Segment
-              textAlign="center"
-              vertical
-              style={{ marginBottom: "40px", paddingBottom: "40px" }}
-            >
-              <Container text>
+        </Inner>
+      </SectionWithBackgroundImage>
+
+      <Section background>
+        <StyledInner>
+          {services.map((service, index) => {
+            return (
+              <div>
                 {service.node.tags &&
                   service.node.tags.map(tag => {
                     return (
-                      <Label key={tag} size="large">
-                        <Icon name="tag" /> {tag}
-                      </Label>
+                      <Tag key={tag}>
+                        <BsFillTagFill /> {tag}
+                      </Tag>
                     )
                   })}
-                <Header as="h2" style={{ marginTop: "15px" }}>
-                  {service.node.title}
-                </Header>
-                <div style={{ marginBottom: "20px" }}>
-                  {service.node.shortDescription.shortDescription}
-                </div>
+                <h3>{service.node.title}</h3>
+                <p>{service.node.shortDescription.shortDescription}</p>
                 <Link to={`/services/${service.node.slug}`}>
-                  <Button
-                    positive
-                    content="Läs mer"
-                    icon="arrow right"
-                    labelPosition="left"
-                  />
+                  <Button>Läs mer</Button>
                 </Link>
-              </Container>
-            </Segment>
-          )
-        })}
-      </Segment>
+              </div>
+            )
+          })}
+        </StyledInner>
+      </Section>
     </Layout>
   )
 }
 
 export default Services
+
+const StyledInner = styled(Inner)`
+  display: grid;
+  grid-gap: 50px;
+
+  > div {
+    box-shadow: 0px 4px 4px rgba(135, 146, 161, 0.16),
+      0px 6px 41px rgba(135, 146, 161, 0.11);
+    border-radius: 18px;
+    margin-bottom: 20px;
+    background-color: #fff;
+    color: var(--color-heading);
+    text-align: center;
+    padding: 20px 30px 15px 30px;
+  }
+
+  p {
+    margin-left: auto;
+    margin-right: auto;
+  }
+
+  @media screen and (max-width: 600px) {
+    h3 {
+      font-size: 24px;
+    }
+  }
+`
