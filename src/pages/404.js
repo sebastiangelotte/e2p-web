d1cb3a0b375fd592a49f2a25bbd994295ee90eff
Sebastian Gelotte <sebastian.gelotte@gmail.com>
2020-10-11 14:11:55 +0200

    Better 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,54 @@
 import React from "react"
-
+import Head from "../components/head"
 import Layout from "../components/layout"
+import styled from "styled-components"
+import { Link } from "gatsby"
+import bg from "../images/hero-bg.svg"
+import {
+  Section,
+  Heading,
+  SectionWithBackgroundImage,
+  Inner,
+  Button,
+} from "../components/new/styledComponents"
 
 const NotFoundPage = () => (
-  <Layout>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+  <Layout transparentNavigation>
+    <Head title="404: Hittar ej sidan" />
+    <SectionWithBackgroundImage backgroundImage={bg} firstSection>
+      <StyledInner>
+        <Heading as="h1" inverted>
+          404: Hittar ej sidan
+        </Heading>
+      </StyledInner>
+    </SectionWithBackgroundImage>
+    <StyledSection background>
+      <StyledInner>
+        <Link to="/">
+          <Button>Ta mig hem</Button>
+        </Link>
+      </StyledInner>
+    </StyledSection>
   </Layout>
 )
 
 export default NotFoundPage
+
+const StyledSection = styled(Section)`
+  p {
+    font-size: 20px;
+  }
+
+  h2,
+  h3,
+  h4,
+  h5 {
+    margin-top: 40px;
+  }
+`
+
+const StyledInner = styled(Inner)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
