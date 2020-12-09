17501b998c0a7ccb688f138bf10c717c9b68f3ac
Sebastian Gelotte <sebastian.gelotte@gmail.com>
2020-12-09 17:54:52 +0100

    Share image

--- src/components/head.js
+++ src/components/head.js
@@ -1,8 +1,28 @@
 import React from "react"
 import { Helmet } from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
+import getShareImage from "@jlengstorf/get-share-image"
 
 const Head = ({ title, description }) => {
+  const socialImage = getShareImage({
+    title: title,
+    tagline: description,
+    cloudName: "e2p",
+    imagePublicID: "share-image",
+    titleFont: "Lato",
+    taglineFont: "Lato",
+    textColor: "11033E",
+    textAreaWidth: 631,
+    textLeftOffset: 567,
+    titleFontSize: 44,
+    taglineFontSize: 30,
+    titleBottomOffset: 310,
+    taglineTopOffset: 310,
+    imageWidth: 1280,
+    imageHeight: 576,
+    titleExtraConfig: "_bold",
+  })
+
   const data = useStaticQuery(graphql`
     query {
       site {
@@ -18,6 +38,7 @@ const Head = ({ title, description }) => {
       <title>{`${title} | ${data.site.siteMetadata.title}`}</title>
       <meta name="description" content={description} />
       <meta name="robots" content="index, follow" />
+      <meta name="image" content={socialImage} />
       {/* <meta property="og:type" content="article" /> */}
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
