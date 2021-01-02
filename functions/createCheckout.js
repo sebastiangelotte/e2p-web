be55e4711b3277e1f3e0781ca037ebc2415cd4a7
Sebastian Gelotte <sebastian.gelotte@gmail.com>
2021-01-02 16:58:09 +0100

    Success URL now dynamic

--- functions/createCheckout.js
+++ functions/createCheckout.js
@@ -1,14 +1,15 @@
+// TODO: can we get price from Contentful instead? If so, there's no need to create products in Stripe which would be niiiice.
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
 
 exports.handler = async event => {
   const body = JSON.parse(event.body)
-  const { cancelUrl, name, priceId, image, metadata } = body
+  const { successUrl, cancelUrl, name, priceId, image, metadata } = body
   const { currency, unit_amount: amount } = await stripe.prices.retrieve(
     priceId
   ) // get price from stripe to avoid scamz
 
   const session = await stripe.checkout.sessions.create({
-    success_url: `https://www.easy2perform.se`,
+    success_url: successUrl,
     cancel_url: cancelUrl,
     payment_method_types: ["card"],
     line_items: [
