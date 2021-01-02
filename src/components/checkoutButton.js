418849d8b05a70bd5cf363ed2bc80e88dffa4a84
Sebastian Gelotte <sebastian.gelotte@gmail.com>
2021-01-02 16:58:49 +0100

    Style checkout button and fix window error and provide success URL

--- src/components/checkoutButton.js
+++ src/components/checkoutButton.js
@@ -1,10 +1,18 @@
 import React from "react"
 import { loadStripe } from "@stripe/stripe-js"
 import getShareImage from "@jlengstorf/get-share-image"
+import styled from "styled-components"
+import { Button } from "./styledComponents"
 
 const stripePromise = loadStripe("pk_test_iMg3jrAGkCY8LZezFr9ndteq") // publishable key, no worries
 
-const CheckoutButton = ({ priceId, name, shortDescription, metadata }) => {
+const CheckoutButton = ({
+  priceId,
+  name,
+  shortDescription,
+  metadata,
+  children,
+}) => {
   const image = getShareImage({
     title: name,
     tagline: shortDescription,
@@ -25,7 +33,11 @@ const CheckoutButton = ({ priceId, name, shortDescription, metadata }) => {
   })
 
   const data = {
-    cancelUrl: document.location.href, // back to previous page if error
+    successUrl:
+      typeof window !== "undefined"
+        ? window.location.origin + "/orderConfirmation"
+        : null,
+    cancelUrl: typeof window !== "undefined" ? window.location.href : null, // back to previous page if error
     name,
     priceId,
     image,
@@ -37,12 +49,21 @@ const CheckoutButton = ({ priceId, name, shortDescription, metadata }) => {
       method: "POST", // needed to be able to send a body
       body: JSON.stringify(data),
     }).then(res => res.json())
-
     const stripe = await stripePromise
     stripe.redirectToCheckout({ sessionId: response.id })
   }
 
-  return <button onClick={redirectToCheckout}>Checkout</button>
+  return <StyledButton onClick={redirectToCheckout}>{children}</StyledButton>
 }
 
 export default CheckoutButton
+
+const StyledButton = styled(Button)`
+  background: linear-gradient(180deg, #fbc917 0%, #ff8364 100%);
+  border: none;
+  font-weight: bold;
+  margin-bottom: 0;
+  margin-top: 10px;
+  width: 100%;
+  font-size: 17px;
+`
