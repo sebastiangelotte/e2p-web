3939644d6d85ccbb4191e47cb41633e226adbd6f
Eric Andersson <eric.andersson88@outlook.com>
2021-01-05 22:34:57 +0100

    Refactor addUser function

--- functions/addUser.js
+++ functions/addUser.js
@@ -10,57 +10,89 @@ exports.handler = async event => {
 
   // const body = JSON.parse(event.body)
   const { email, name } = {
-    email: "aoa@pasd.se",
-    name: "dsdsd",
+    email: "Gustaff@pasd.se",
+    name: "Gustaff Wasa",
   }
 
   const { course } = {
     course: "Testkurs",
   }
 
-  base("Orders").create([
-    {
-      fields: {
-        Course: course,
-        Created: Date.now(),
-        PaymentMethod: "Card",
-        Status: "Completed",
-      },
-    },
-  ])
-  base("Users")
-    .select({
-      filterByFormula: `email = "${email}"`,
+  function getUserRecords() {
+    return new Promise((resolve, reject) => {
+        base("Users")
+      .select({
+        filterByFormula: `email = "${email}"`,
+      })
+      .firstPage(
+        (err, records) => {
+          if (err) {
+            reject(err)
+          }
+          resolve(records)
+        }
+      )
     })
-    .eachPage(
-      records => {
-        if (records.length === 0) {
-          // if user doesn't exist, create it
-          base("Users").create(
-            [
-              {
-                fields: {
-                  Email: email,
-                  Name: name,
-                },
-              },
-            ],
-            err => {
-              if (err) {
-                console.log(err)
-                return
-              }
-            }
-          )
+  }
+  
+  function createUser() {
+    return new Promise((resolve, reject) => {
+      base("Users").create(
+        [
+          {
+            fields: {
+              Email: email,
+              Name: name,
+            },
+          },
+        ],
+        (err, records) => {
+          if (err) {
+            console.log("Error creating user")
+            console.log(err)
+            reject(err)
+          }
+          if (records.length === 0) {
+            console.log("Records.length is 0 after creating new user")
+            reject(err)
+          }
+          record = records[0]
+          userId = record.getId()
+  
+          resolve(userId)
         }
+      )
+    })
+  }
+
+  const createOrder = async userId => {
+    await base("Orders").create([
+      {
+        fields: {
+          Course: course,
+          Created: Date.now(),
+          PaymentMethod: "Card",
+          Status: "Completed",
+          User: [userId],
+        },
       },
-      (err, records) => {
-        if (err) {
-          console.log(err)
-          return
-        }
-      }
-    )
+    ])
+  }
+  
+  records = await getUserRecords()
+  if (records.length === 0) {
+    // if user doesn't exist, create it
+    const userId = await createUser()
+    if (!userId) {
+      return
+    }
+  }
+  else {
+    const record = records[0]
+    const userId = records.getId()
+  }
+
+  await createOrder(userId)
 
   return {
     statusCode: 200,
