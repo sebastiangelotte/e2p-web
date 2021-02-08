8765abb805418085f598ac1f9d5282f71f330860
Seb <seb@pop-os.localdomain>
2021-02-08 21:30:30 +0100

    Remove stripePriceId from query

--- src/templates/companyInternalCourse.js
+++ src/templates/companyInternalCourse.js
@@ -127,7 +127,6 @@ export const query = graphql`
         }
         createdAt(formatString: "DD MMM, YYYY")
       }
-      stripePriceId
     }
   }
 `
--- src/templates/openCourse.js
+++ src/templates/openCourse.js
@@ -138,7 +138,6 @@ export const query = graphql`
           lon
         }
       }
-      stripePriceId
     }
   }
 `
